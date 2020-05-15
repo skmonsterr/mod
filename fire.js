ed808d0680d63f346586a78a4064be4aa2d036fc
sKm <contatojoaomartins@yahoo.com>
2020-05-15 02:52:51 -0300

    Added Data Fire Structure

---
+++ fire.js
@@ -0,0 +1,28 @@
+const firePixelsArray = []
+const fireWidth = 2
+const fireHeight = 3
+
+function start() {
+    createFireDataStructure()
+    console.log(firePixelsArray)
+}
+
+function createFireDataStructure() {
+    const numberOfPixels = fireWidth * fireHeight
+
+    for (let i = 0; i < numberOfPixels; i++) {
+        firePixelsArray[i] = 0
+    }
+
+}
+s
+
+function calculateFirePropagation() {
+
+}
+
+function renderFire() {
+
+}
+
+start()
